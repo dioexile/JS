48a6767da7d067b705f796408722338c03abe0b3
dioexile <45012858+dioexile@users.noreply.github.com>
2021-10-04 17:15:42 +0300

    2 lesson

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,19 @@
-alert('DAS')
+let title
+let screens
+let screenPrice
+let rollback
+let fullPrice
+let titladaptivee
+
+title = 'Document';
+screens ='Простые, Сложные, Интерактивные';
+screenPrice = 10000;
+adaptive=true;
+fullPrice = 1000000;
+rollback = Math.floor(Math.random() * 101)
+console.log(rollback)
+console.log(typeof fullPrice, typeof adaptive, typeof title, screens.length)
+console.log(screens.toLowerCase().split(", "))
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей')
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей')
+console.log(fullPrice * (rollback/100))
