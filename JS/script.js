c9b5c662ea44709d93eff0afb0ef59e08395e491
dioexile <45012858+dioexile@users.noreply.github.com>
2021-10-05 12:10:07 +0300

    lesson03

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,27 @@
-alert('DAS')
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?');
+let screenPrice = prompt('Сколько будет стоить данная работа?');
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+let rollback = Math.floor(Math.random() * 101);
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = prompt('Сколько это будет стоить?');
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - fullPrice * (rollback/100);
+
+if (fullPrice>30000 ) {
+  alert('Даем скидку в 10%');
+} else if(fullPrice >= 15000 && fullPrice <= 30000){
+    alert('Даем скидку в 5%');
+}
+  else if(fullPrice <= 15000 && fullPrice >= 0){
+    alert('Скидка не предусмотрена');
+  }
+  else if(fullPrice <=0){
+    alert('Что то пошло не так');
+  }
+
+
+// console.log(screens)
