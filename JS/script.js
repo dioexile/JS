38613297783df0aeed414f2aeee5f109d2dbfbaa
dioexile <45012858+dioexile@users.noreply.github.com>
2021-10-11 04:27:40 +0300

    06

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,23 @@
-alert('DAS')
+let num = Math.floor(Math.random() * 101);
+let attempt
+
+const isNumber =function(num){
+  return !isNaN(parseFloat(num)) && isFinite(num)
+}
+
+function game(){
+  while(attempt!=num){
+    attempt = +prompt("Угадай число от 1 до 100")
+    if (attempt < num) {
+      alert("Загаданное число больше")
+    } else if (attempt > num){
+      alert("Загаданное число меньше" )
+    } else if(!isNumber(attempt)){
+      alert("Введи число!")
+    }
+    else if(attempt==num){
+      alert("Поздравляю, Вы угадали!!!")
+    }
+  }
+}
+game()
